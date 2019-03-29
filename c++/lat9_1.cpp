ac470a8b30757bf87fa5b6e2c5c01c5ccc162087
djandatua <40305086+djandatua@users.noreply.github.com>
2019-03-30 01:50:36 +0700

    Add files via upload

---
+++ c++/lat9_1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+int main (void)
+{
+char c ='1';
+if (isalpha (c))
+printf ("%c is alphabetical \n",c);
+else printf ("%c is not alphabelcal\n",c);
+return 0;
+}
---
+++ c++/lat9_3.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+int main (void)
+{
+char c='c';
+if (islower (c))
+printf ("%c is lower case character \n",c);
+else printf ("%c is not lower case character\n",c);
+return 0;
+}
